9f0d4d7c4231be681b43967852f0a6e4a2223a13
健勇 刘 <648125653@qq.com>
2014-03-27 19:14:41 +0800

    html首页制作，创建resouce静态资源目录

--- application/views/main.php
+++ application/views/main.php
@@ -3,9 +3,72 @@
 <head>
 	<title>An XHTML 1.0 Strict standard template</title>
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-
+	<link rel="stylesheet" type="text/css" href="/resource/css/comm.css">
+	<script type="text/javascript" async="" charset="utf-8" src="/resource/js/jquery-1.11.0.min.js"></script>
 </head>
 <body>
-	
+	<div class="index-header">
+		<div class="container">
+			<a class="logo" href="">WeAreCoser</a>
+			<a class="button-action">登陆</a>
+			<ul class="menu">
+				<li><a href="">cos美图</a></li>
+				<li><a href="">cos社团</a></li>
+			</ul>
+			
+			<div class="clear"></div>
+		</div>
+	</div>
+	<div class="index-markblock">
+		<div class="container">
+			<div class="info-box">
+				<h1>让我们尽情享受cosplay的乐趣吧～</h1>
+			</div>
+			<div class="register-box">
+				<form>
+					<dl>
+						<dd><input type="text"  placeholder="填写你的邮箱地址"></input></dd>
+						<dd><input type="text"  placeholder="使用的昵称"></input></dd>
+						<dd><input type="text"  placeholder="登陆密码"></input></dd>
+					</dl>
+					<button class="button" type="submit">注册成为我们的一员</button>
+				</form>
+			</div>
+		</div>	
+	</div>
+	<div class="index-businessblock">
+		<div class="container">
+			<p class="title">希望你能喜欢我们的理由:</p>
+			<table  cellspacing="0" cellpadding="0" border="0">
+				<tbody>
+					<tr>
+						<td>
+							<div class="business-icon icon1"></div>
+							<p>这里有全国各地志同道合的人，加入他们一起做更多有意思的事。</p>
+						</td>
+						<td>
+							<div class="business-icon icon2"></div>
+							<p>你可以创建属于你的社团，管理招募伙伴们一起玩cosplay。</p>
+						</td>
+						<td>
+							<div class="business-icon icon3"></div>
+							<p>这里能找到丰富的cosplay优秀作品欣赏，分享。</p>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<div class="business-icon icon4"></div>
+							<p>cos社团能在这里找到更多参展活动机会，赚得酬金。</p>
+						</td>
+						<td>
+							<div class="business-icon icon5"></div>
+							<p>cos社团之间可以发布,交易cos道具，服装等资源。满足社团活动需要。</p>
+						</td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
 </body>
 </html>
